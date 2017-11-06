aaaeb7ba40ebd8d220e2847a889631a6bdee5cd8
Unknown <imran.pavel@gmail.com>
2017-11-06 23:35:13 +0200

    Leaderboard Page Implemented
    This page was not fully implemented on our last meeting.
    
    This tutorial has been followed to implement the progress bar of this page: https://www.youtube.com/watch?v=GfuxWs6UAJQ

---
+++ Assets/ProgressBarScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarScript : MonoBehaviour {
+    public float MaxNHotSpots;
+    public float NCollectedHotSpots;
+
+    public Slider ProgressBar;
+	// Use this for initialization
+	void Start () {
+        MaxNHotSpots = 5f;
+        NCollectedHotSpots = 0f;
+
+        ProgressBar.value = CurrentProgress();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.X))
+        {
+            NewHotSpotReached();
+        }
+	}
+
+    void NewHotSpotReached()
+    {
+        if (NCollectedHotSpots >= MaxNHotSpots)
+        {
+            return;
+        }
+        NCollectedHotSpots++;
+        ProgressBar.value = CurrentProgress();
+    }
+
+    float CurrentProgress()
+    {
+        return NCollectedHotSpots / MaxNHotSpots;
+    }
+}
